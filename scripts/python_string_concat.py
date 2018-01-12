b5e16344e04f0f75ede3bd231902e7d44f9f3ff4
papicrunch <papicrunch@gmail.com>
2018-01-12 10:08:22 +0100

    update structure

--- scripts/python_string_concat.py
+++ scripts/python_string_concat.py
@@ -1,13 +1,22 @@
 node = hou.pwd()
 geo = node.geometry()
-geo.addAttrib(hou.attribType.Global, "allInstance2", "")
+
 from cStringIO import StringIO
 file_str = StringIO()
 
+##geo.addAttrib(hou.attribType.Global, "allInstance2", "")
+loc = "F:/test.ass"
+
 for point in geo.points():
-    tmp_list = point.attribValue("declareProc")
+    tmp_list = point.attribValue("declareInstance")
     file_str.write(tmp_list)
-    
-value = file_str.getvalue()
-geo.setGlobalAttribValue("allInstance2", value)
+
+f=open(loc, "w")
+f.write(file_str.getvalue())
+f.close()
+
+## value = file_str.getvalue()
+##geo.setGlobalAttribValue("allInstance2", value)
+
+
 file_str.close()
