87bc078722b3ee1790a0225b51194e97319256b4
papicrunch <papicrunch@gmail.com>
2018-07-06 15:46:05 +0200

    add new driver method

---
+++ hmtc/scripts/python/HMTCutils.py
@@ -0,0 +1,16 @@
+def setTakeDriver(node):
+
+	n = node
+	sNoTake = """$IMAGES/fx/render/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("version")`"""
+	sTake = """$IMAGES/fx/render/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("take")`_`chs("version")`"""
+	sNoTakeIfds = """$JOB/ifds/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("version")`"""
+	sTakeIfds = """$JOB/ifds/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("take")`_`chs("version")`"""
+	
+
+	if(n.parm('useTake').eval()):
+		n.parm("outImages").set(sTake)
+		n.parm("outIfds").set(sTakeIfds)
+
+	else:
+		n.parm("outImages").set(sNoTake)
+		n.parm("outIfds").set(sNoTakeIfds)
