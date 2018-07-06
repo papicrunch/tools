55cd0bb0ecd0af961bc64fca21d6a6edc83d3945
papicrunch <papicrunch@gmail.com>
2018-07-06 17:20:36 +0200

    update methode

--- hmtc/scripts/python/HMTCutils.py
+++ hmtc/scripts/python/HMTCutils.py
@@ -14,3 +14,20 @@ def setTakeDriver(node):
 	else:
 		n.parm("outImages").set(sNoTake)
 		n.parm("outIfds").set(sNoTakeIfds)
+
+def setTakeArnold(node):
+
+	n = node
+	sNoTake = """$IMAGES/fx/render/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("version")`"""
+	sTake = """$IMAGES/fx/render/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("take")`_`chs("version")`"""
+	sNoTakeIfds = """$JOB/ass/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("version")`"""
+	sTakeIfds = """$JOB/ass/`chs("asset")`/`chs("version")`/`chs("asset")`_`chs("take")`_`chs("version")`"""
+	
+
+	if(n.parm('useTake').eval()):
+		n.parm("outImages").set(sTake)
+		n.parm("outAss").set(sTakeIfds)
+
+	else:
+		n.parm("outImages").set(sNoTake)
+		n.parm("outAss").set(sNoTakeIfds)
